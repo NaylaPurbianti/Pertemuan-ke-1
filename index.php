18c92216c5d50357a62234834733246257b9891b
NaylaPurbianti <naylaapriliaarmanapurbianti@gmail.com>
2024-11-13 18:23:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$nama = "Nayla";
+$umur = "18";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Contoh Variable PHP</title>
+</head>
+<body>
+      <h1>Hallo, <?php echo $nama; ?>!</h1>
+      <p>Umur Anda adalah <?php echo $umur; ?> tahun.</p>
+</body>
+</html>
